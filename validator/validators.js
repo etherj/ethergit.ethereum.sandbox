7cc4869268b610f5b87aa7b182150c1c20a38d59
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-09-08 16:53:54 +0000

    Fix the problem with validators in packed mode (workaround for https://github.com/c9/core/issues/145)

--- validator/validators.js
+++ validator/validators.js
@@ -1,32 +1,31 @@
-define(
-    ['./uintN', './intN', './bytesN', './address', './string', './bool', './bytes', './array'],
-    function(
-        uintNValidator,
-        intNValidator,
-        bytesNValidator,
-        addressValidator,
-        stringValidator,
-        boolValidator,
-        bytesValidator,
-        arrayValidator
-    ) {
-        var dummyValidator = {
-            validate: function(value) {
-                console.error('Using dummy validator');
-                return [];
-            }
-        };
-        
-        return function(type) {
-            if (type === 'address') return addressValidator;
-            if (type === 'string') return stringValidator;
-            if (type === 'bool') return boolValidator;
-            if (type === 'bytes') return bytesValidator;
-            if (/^uint\d+$/.test(type)) return uintNValidator(type);
-            if (/^int\d+$/.test(type)) return intNValidator(type);
-            if (/^bytes\d+$/.test(type)) return bytesNValidator(type);
-            if (_.endsWith(type, ']')) return arrayValidator(type);
-            return dummyValidator;
-        };
-    }
-);
+define([
+    './uintN', './intN', './bytesN', './address', './string', './bool', './bytes', './array'
+], function(
+    uintNValidator,
+    intNValidator,
+    bytesNValidator,
+    addressValidator,
+    stringValidator,
+    boolValidator,
+    bytesValidator,
+    arrayValidator
+) {
+    var dummyValidator = {
+        validate: function(value) {
+            console.error('Using dummy validator');
+            return [];
+        }
+    };
+    
+    return function(type) {
+        if (type === 'address') return addressValidator;
+        if (type === 'string') return stringValidator;
+        if (type === 'bool') return boolValidator;
+        if (type === 'bytes') return bytesValidator;
+        if (/^uint\d+$/.test(type)) return uintNValidator(type);
+        if (/^int\d+$/.test(type)) return intNValidator(type);
+        if (/^bytes\d+$/.test(type)) return bytesNValidator(type);
+        if (_.endsWith(type, ']')) return arrayValidator(type);
+        return dummyValidator;
+    };
+});
