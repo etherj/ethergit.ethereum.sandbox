791c71656f941354f9498d551d01fe8fe2ac55c4
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2016-04-09 20:03:17 +0000

    Add a feature to allow user publish and protect his project

---
+++ publish/publish.js
@@ -0,0 +1,70 @@
+define(function(require, exports, module) {
+  main.consumes = ['Plugin', 'ui', 'api', 'info', 'menus', 'dialog.confirm'];
+  main.provides = ['ethereum.studio.publish'];
+  return main;
+
+  function main(options, imports, register) {
+    var Plugin = imports.Plugin;
+    var ui = imports.ui;
+    var api = imports.api;
+    var info = imports.info;
+    var menus = imports.menus;
+    var confirm = imports['dialog.confirm'].show;
+
+    var plugin = new Plugin('ether.camp', main.consumes);
+
+    var menuItem;
+
+    plugin.on('load', function() {
+      info.getWorkspace(function(err, project) {
+        info.getUser(function(err, user) {
+          if (user.access != 'ADMIN') return;
+          menuItem = new ui.item({ onclick: publishOrProtect });
+          menus.addItemByPath(
+            'user_' + user.id + '/' + getLabel(project.nonpublic),
+            menuItem,
+            675,
+            plugin
+          );
+        });
+      });
+    });
+
+    function getLabel(nonpublic) {
+      return nonpublic ? 'Publish project' : 'Protect project';
+    }
+
+    function publishOrProtect() {
+      info.getWorkspace(function(err, project) {
+        var title, head, msg;
+        
+        if (project.nonpublic) {
+          title = 'Publish project?';
+          head = 'Do you want to publish the project?';
+          msg = 'Anyone will be able to see your project in readonly mode.';
+        } else {
+          title = 'Protect project?';
+          head = 'Do you want to protect the project?';
+          msg = 'Only project members will be able to see your project.';
+        }
+        
+        confirm(title, head, msg, function() {
+          api.project.post('/publish', {
+            contentType: 'application/json',
+            body: JSON.stringify({
+              nonpublic: !project.nonpublic
+            })
+          }, function(err, session, res) {
+            if (err) return console.error(err);
+
+            project.nonpublic = JSON.parse(res.body).nonpublic;
+            menuItem.setAttribute('caption', getLabel(project.nonpublic));
+          });
+        });
+      });
+    }
+    
+    plugin.freezePublicAPI({});
+    register(null, { 'ethereum.studio.publish': plugin });
+  }
+});
