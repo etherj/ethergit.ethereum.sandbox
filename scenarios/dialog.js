c5e71d0c7cc7abe053cbb451a239cb6a153cc4e0
Alex Sinyagin <sinyagin.alexander@gmail.com>
2016-12-30 14:59:53 +0000

    Read scenarios and show them in the scenarios dialog

--- scenarios/dialog.js
+++ scenarios/dialog.js
@@ -1,6 +1,6 @@
 define(function(require) {
   main.consumes = [
-    'Dialog', 'ui', 'layout', 'commands', 'menus', 'Menu',
+    'Dialog', 'ui', 'layout', 'commands', 'menus', 'Menu', 'fs',
     'ethergit.libs', 'ethergit.sandbox'
   ];
   main.provides = ['ethergit.dialog.scenarios'];
@@ -13,12 +13,16 @@ define(function(require) {
     var commands = imports.commands;
     var menus = imports.menus;
     var Menu = imports.Menu;
+    var fs = imports.fs;
     var libs = imports['ethergit.libs'];
     var sandbox = imports['ethergit.sandbox'];
 
+    var async = require('async');
+    
     var $ = libs.jquery();
+    var _ = libs.lodash();
 
-    var $scenarios;
+    var $scenarios, $error;
 
     var dialog = new Dialog('Ethergit', main.consumes, {
       name: 'ethergit-dialog-scenarios',
@@ -74,11 +78,46 @@ define(function(require) {
       e.html.innerHTML = require('text!./dialog.html');
       var $root = $(e.html);
       $scenarios = $root.find('[data-name=scenarios]');
+      $error = $root.find('[data-name=error]');
     });
 
     dialog.on('show', function() {
       $scenarios.empty();
-      $scenarios.append('Scenario1');
+      $error.empty();
+      
+      sandbox.web3.sandbox.getProjectDir(function(err, projectDir) {
+        if (err) return console.error(err);
+
+        var scenariosDir = projectDir + 'scenarios/';
+        
+        fs.exists(scenariosDir, function(exists) {
+          if (!exists) {
+            $error.text('There is no directory ' + scenariosDir);
+            return;
+          }
+            
+          fs.readdir(scenariosDir, function(err, files) {
+            if (err) return console.error(err);
+            files = _.filter(files, function(file) {
+              return _.endsWith(file.name, '.json');
+            });
+            async.map(files, function(file, cb) {
+              fs.readFile(scenariosDir + file.name, function(err, content) {
+                if (err) return cb(err);
+                cb(null, {
+                  name: file.name.substr(0, file.name.length - 5),
+                  content: content
+                });
+              });
+            }, function(err, scenarios) {
+              if (err) return $error.text(err);
+              _.each(scenarios, function(scenario) {
+                $scenarios.append('<li>' + scenario.name + '</li>');
+              });
+            });
+          });
+        });
+      });
     });
     
     function hide() {
